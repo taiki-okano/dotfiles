dc1911551834ecd2c0e2116932f2852499262139
Taiki Okano <taiki@okano.tokyo>
2025-12-17 17:57:46 +0100

    Expand tab in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,5 +6,6 @@ vim.opt.number = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.expandtab = true
 
 vim.keymap.set("n", "<Space>h", vim.cmd.nohlsearch, { silent = true })
